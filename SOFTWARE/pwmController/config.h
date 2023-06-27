720df52df2bee3ee160ba5c94af9d96552ed3a55
bask185 <sebastiaan_knippels4@hotmail.com>
2023-06-27 13:46:44 +0200

    added Lopits source as well as new PWM contoller

---
+++ SOFTWARE/pwmController/config.h
@@ -0,0 +1,14 @@
+
+#include <Arduino.h>
+
+const int currentLimit       =  10 ; // CHECK ME< ADD CALCULATION
+const int speedPin           =  A0 ;
+const int currentSensePin    =  A1 ;
+const int rs485dir           =   2 ; // Xnet control pin
+const int in1                =  10 ; // h brug
+const int in2                =  11 ; // h brug
+const int Fmin               =  50 ;
+const int Fmax               = 100 ;
+const int accelerationFactor =  50 ; // smaller -> faster acceleration, bigger -> slower acceleration
+
+// #define HAS_XPRESSNET
---
+++ SOFTWARE/pwmController/pwmController.ino
@@ -0,0 +1,47 @@
+#include "src/macros.h"
+#include "src/weistra.h"
+#include "src/XpressNetMaster.h"
+#include "config.h"
+
+
+Weistra pwm( in1, in2, Fmin, Fmax ) ; // pin 10 and 11 for direction, Fmin = 50, Fmax = 100
+
+#ifdef HAS_XPRESSNET
+XpressNetMasterClass Xnet ;
+#endif
+
+void setup()
+{
+    pwm.begin() ;
+    pwm.setCurrentSense( currentSensePin, currentLimit ) ;
+
+#ifdef HAS_XPRESSNET
+    Xnet.setup( Loco128, rs485dir ) ;
+#endif
+}
+
+void loop()
+{
+    pwm.update() ;
+
+#ifdef HAS_XPRESSNET
+    Xnet.setup( Loco128, rs485dir ) ;                       // either use Xnet
+#else
+
+    REPEAT_MS( accelerationFactor )                         // or use potentiometer
+    {
+        int setPoint = analogRead( speedPin ) ;
+        static int currentSpeed = 0 ;
+
+        setPoint = map( setPoint, 0, 1023, -100, 100 ) ;
+
+        if( setPoint > currentSpeed ) currentSpeed ++ ;
+        if( setPoint < currentSpeed ) currentSpeed -- ;
+
+        pwm.setSpeed( currentSpeed ) ;
+
+    }
+    END_REPEAT
+#endif
+}
+
