7fe6398cefa5e6e97c8a26c89d683bd368590f16
bask185 <sebastiaan_knippels4@hotmail.com>
2023-07-03 13:48:21 +0200

    added a minimum speed option. I still need to let the acceleration control lockout the deadzone created by the min speed

--- SOFTWARE/pwmController/config.h
+++ SOFTWARE/pwmController/config.h
@@ -1,4 +1,3 @@
-
 #include <Arduino.h>
 
 // maximum current
@@ -6,10 +5,11 @@ const int currentLimit       =  10 ; // CHECK ME ADD CALCULATION
 
 // used IO PINs
 const int speedPin           =  A0 ;
-const int maxSpeedPin        =  A1 ;
-const int accelFactorPin     =  A2 ;
-const int waitTimePin        =  A3 ;
-const int currentSensePin    =  A4 ;
+const int currentSensePin    =  A1 ;
+const int minSpeedPin        =  A2 ;
+const int maxSpeedPin        =  A3 ;
+const int accelFactorPin     =  A4 ;
+const int waitTimePin        =  A5 ;
 const int in1                =  10 ; // h brug
 const int in2                =  11 ; // h brug
 const int S1                 =   3 ;
@@ -19,7 +19,8 @@ const int S2                 =   4 ;
 const int Fmin               =  50 ;
 const int Fmax               = 100 ;
       int accelerationFactor =  50 ;
+      int minSpeed           =   1 ; // minimum speed. Slow trains start driving at higher throttle levers
       int maxSpeed           = 100 ; // may NOT be bigger than 100
 
 // shuttle service station wait time
-uint32_t    waitTime = 15000 ; // 15 seconds
+uint32_t    waitTime = 30000 ; // 30 seconds
